9fee1bfde65771d69ca0dc1d4385b2f5022ce829
Sebastian Puetz <sebastian.puetz@student.uni-tuebingen.de>
2019-10-01 18:00:19 +0200

    Run pytest against python package.

--- ci/script.sh
+++ ci/script.sh
@@ -2,4 +2,9 @@
 
 set -euxo pipefail
 
-python setup.py build_ext --test
+python setup.py build_ext --test install --verbose
+
+pushd tests
+pytest test_eager_mode.py
+pytest test_graph_mode.py
+popd
